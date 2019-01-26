c2fbd0c0aee4fbe8a41a9311fc3a9b6dd75840ef
GalindoA00 <AldoJ.galindo@gmail.com>
2019-01-26 11:27:35 -0800

    1st Documented

---
+++ Game Project/Assets/Scripting/PLAYERCONTROLLER.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYERCONTROLLER : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+		if(Input.GetKeyDown("space") && rb.transform.position.y <= 0.51f)
+		{
+			Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+			rb.AddForce(jump);
+		}
+	}
+}
