b29b294c7b31e6d9d4c9cdcdf933fe3a574cdc09
GalindoA00 <AldoJ.galindo@gmail.com>
2019-03-13 14:07:29 -0700

    Camera Cintroller Update

--- Game Project/Assets/Scripting/CameraController.cs
+++ Game Project/Assets/Scripting/CameraController.cs
@@ -2,21 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController: MonoBehaviour
+public class CameraController : MonoBehaviour
 {
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
 
-    public GameObject player;
+    [SerializeField] private Transform playerBody;
 
-    private Vector3 offset;
-    // Use this for initialization
-    void Start()
+    private float xAxisClamp;
+
+    private void Awake()
     {
-        offset = transform.position - player.transform.position;
+        LockCursor();
+        xAxisClamp = 0.0f;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+
+    private void LockCursor()
     {
-        transform.position = player.transform.position + offset;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX); //something wrong with this that causes jumping
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
     }
 }
--- Game Project/Assets/Scripting/PLAYERCONTROLLER.cs
+++ Game Project/Assets/Scripting/PLAYERCONTROLLER.cs
@@ -5,30 +5,93 @@ using UnityEngine;
 public class PLAYERCONTROLLER : MonoBehaviour
 {
 
-    public float speed;
+   /* public float speed;//Aldo Code for Player Controller 
 
-    private Rigidbody rb;
+    private Rigidbody rb; //References from Unity RigiBody Component, creates the variable to hold the reference 
 
     // Use this for initialization
-    void Start()
+    void Start() //Run when the game starts, Where all the game code will go
     {
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //Physics Code will go
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");//Grabs our input of our player through the keyboard. Which the vertical and Horizontal Axis is controlled throught the keyboard 
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); //Vector3 is (x,y,z), Moving with left and right key and y is not move but to stay still 
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed); //Addforce adds force to the rigid body 
 
-		if(Input.GetKeyDown("space") && rb.transform.position.y <= 0.51f)
+        if (Input.GetKeyDown("space")) //&& rb.transform.position.y <= 0.51f) //Hwanyi Code to Jump
 		{
-			Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+			Vector3 jump = new Vector3(0.0f, 500.0f, 0.0f);
 			rb.AddForce(jump);
 		}
-	}
+	}*/
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+
 }
